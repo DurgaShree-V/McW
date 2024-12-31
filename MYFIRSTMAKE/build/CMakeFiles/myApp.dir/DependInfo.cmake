bf2aaa3cd478794a66b250629c5f77904a1a7133
DurgaShree-V <durgathepro@gmail.com>
2024-12-31 12:41:47 +0530

    Created Cmake

---
+++ MYFIRSTMAKE/build/CMakeFiles/myApp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/nithy/OneDrive/Desktop/V.P.D.S/Learn_Github/MYFIRSTMAKE/hello.cpp" "CMakeFiles/myApp.dir/hello.cpp.o" "gcc" "CMakeFiles/myApp.dir/hello.cpp.o.d"
+  "/mnt/c/Users/nithy/OneDrive/Desktop/V.P.D.S/Learn_Github/MYFIRSTMAKE/main.cpp" "CMakeFiles/myApp.dir/main.cpp.o" "gcc" "CMakeFiles/myApp.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MYFIRSTMAKE/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+void hellow();
+
+#endif // HELLO_H
